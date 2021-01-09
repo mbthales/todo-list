880bfe39d9e7e227f769a070737e46a5c9755dde
Thales <mbthales10@gmail.com>
2021-01-09 11:08:18 -0300

    Improvements in the JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,47 @@
 const btnAddTodo = document.querySelector("[data-js=add-todo");
 const inputTodo = document.querySelector("[data-js=todo-content");
-const containerTodo = document.querySelector("[data-js=container-todo");
+const containerTodos = document.querySelector("[data-js=container-todos");
+
+const todos = JSON.parse(localStorage.getItem("todo-content")) || []; 
 
 const putTodoInTheListOfTodos = () => {
-  const todoContent = inputTodo.value;
+  const todo = inputTodo.value;
+  if(todo) todos.push(todo);
+
+  const template = todos.map(todo => `
+    <div class="todo-box">
+    <span data-js="btn-remove" class="btn-close">X</span>
+    <p>${todo}</p>
+    </div>
+  `).join("");
   
-  if(todoContent){
-    const template = `<p>${todoContent}</p>`;
-    containerTodo.innerHTML += template;
-  };
+  containerTodos.innerHTML = template;
+  setTodoInTheStorage();
+  removeTodoInTheListOfTodos();
+};
+
+const removeTodoInTheListOfTodos = () => {
+  const btnsRemoveTodo = document.querySelectorAll("[data-js=btn-remove");
+
+  btnsRemoveTodo.forEach((btn, i) => {
+    btn.addEventListener("click", e => {
+      e.target.parentNode.remove();
+      removeTodoInTheStorage(i);
+    });
+  });
+};
+
+const removeTodoInTheStorage = todo => {
+  todos.splice(todo, 1);
+  localStorage.setItem("todo-content", JSON.stringify(todos));
+}
+
+const setTodoInTheStorage = () => {
+  localStorage.setItem("todo-content", JSON.stringify(todos));
 };
 
 btnAddTodo.addEventListener("click", () => {
   putTodoInTheListOfTodos();
 });
+
+putTodoInTheListOfTodos();
